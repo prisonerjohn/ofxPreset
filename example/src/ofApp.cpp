87f37ce40b6916f64029396672d0ce27d1376297
Elie Zananiri <prisonerjohn@gmail.com>
2016-05-25 17:47:07 -0400

    Added example project with Visual C++ files.

---
+++ example/src/ofApp.cpp
@@ -0,0 +1,356 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	ofSetLogLevel(OF_LOG_NOTICE);
+	ofDisableArbTex();
+	ofBackground(ofColor::black);
+
+	this->stepper = 0.0f;
+
+	// Load default settings, if any.
+	this->loadSettings("defaults.json");
+
+	// GUI
+	this->gui.setup();
+	this->guiVisible = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+	this->stepper += this->parameters.mesh.speed;
+	cubeSize = ofMap(sinf(this->stepper), -1.0f, 1.0f, this->parameters.mesh.sizeMin, this->parameters.mesh.sizeMax);
+	
+	if (this->guiVisible || this->parameters.camera.mouseEnabled)
+	{
+		ofShowCursor();
+	}
+	else
+	{
+		ofHideCursor();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ofBackground(this->parameters.colors.background.get());
+	
+	this->camera.begin();
+	{	
+		ofEnableDepthTest();
+
+		ofSetColor(ofColor::white);
+
+		if (this->parameters.mesh.enabled)
+		{
+			ofPushStyle();
+			{
+				ofSetColor(this->parameters.colors.foreground.get());
+				
+				if (this->parameters.render.fillMode != RenderModeNone)
+				{
+					ofFill();
+					if (this->parameters.render.fillMode == RenderModeTexture && this->texture.isAllocated())
+					{
+						this->texture.bind();
+					}
+					ofDrawBox(this->cubeSize);
+					if (this->parameters.render.fillMode == RenderModeTexture && this->texture.isAllocated())
+					{
+						this->texture.unbind();
+					}
+				}
+				if (this->parameters.render.strokeMode != RenderModeNone)
+				{
+					ofNoFill();
+					if (this->parameters.render.strokeMode == RenderModeTexture && this->texture.isAllocated())
+					{
+						this->texture.bind();
+					}
+					ofDrawBox(this->cubeSize);
+					if (this->parameters.render.strokeMode == RenderModeTexture && this->texture.isAllocated())
+					{
+						this->texture.unbind();
+					}
+				}
+			}
+			ofPopStyle();
+		}
+
+		ofDisableDepthTest();
+	}
+	this->camera.end();
+
+	// GUI
+	this->mouseOverGui = false;
+	if (this->guiVisible)
+	{
+		imGui();
+	}
+	if (this->mouseOverGui)
+	{
+		this->camera.disableMouseInput();
+	}
+	else if (this->parameters.camera.mouseEnabled)
+	{
+		this->camera.enableMouseInput();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+void ofApp::loadCamera()
+{
+	ofFile file(this->parameters.camera.filePath.get());
+	if (file.exists())
+	{
+		nlohmann::json json;
+		file >> json;
+
+		ofxPreset::Serializer::Deserialize(json, this->camera);
+
+		if (this->parameters.camera.mouseEnabled)
+		{
+			this->camera.enableMouseInput();
+		}
+		else
+		{
+			this->camera.disableMouseInput();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::saveCamera()
+{
+	nlohmann::json json;
+
+	ofxPreset::Serializer::Serialize(json, this->camera);
+
+	ofFile file(this->parameters.camera.filePath.get(), ofFile::WriteOnly);
+	file << json;
+}
+
+//--------------------------------------------------------------
+bool ofApp::loadImage(const string & filePath)
+{
+	ofImage image;
+	image.setUseTexture(false);
+	if (!image.load(filePath)) {
+		ofLogError("ofApp::loadImage") << "No image found at " << filePath;
+		return false;
+	}
+
+	ofTextureData texData;
+	texData.width = image.getWidth();
+	texData.height = image.getHeight();
+	texData.textureTarget = GL_TEXTURE_2D;
+	texData.bFlipTexture = true;
+	this->texture.allocate(texData);
+	this->texture.loadData(image.getPixels());
+
+	this->parameters.render.imagePath = filePath;
+	return true;
+}
+
+//--------------------------------------------------------------
+void ofApp::imGui()
+{
+	this->gui.begin();
+	{
+		auto windowPos = ofVec2f(kGuiMargin, kGuiMargin);
+		auto windowSize = ofVec2f::zero();
+
+		if (ofxPreset::Gui::BeginWindow(this->parameters.getName(), windowPos, windowSize))
+		{
+			ImGui::Text("%.1f FPS (%.3f ms/frame)", ofGetFrameRate(), 1000.0f / ImGui::GetIO().Framerate);
+
+			if (ImGui::Button("Save All..."))
+			{
+				auto dialogResult = ofSystemSaveDialog("settings.json", "Save Settings");
+				if (dialogResult.bSuccess)
+				{
+					this->saveSettings(dialogResult.filePath);
+				}
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Load All..."))
+			{
+				auto dialogResult = ofSystemLoadDialog("Load Settings", false, ofToDataPath("settings"));
+				if (dialogResult.bSuccess)
+				{
+					this->loadSettings(dialogResult.filePath);
+				}
+			}
+
+			if (ImGui::CollapsingHeader(this->parameters.colors.getName().c_str(), nullptr, true, true))
+			{
+				ofxPreset::Gui::AddParameter(this->parameters.colors.background);
+				ofxPreset::Gui::AddParameter(this->parameters.colors.foreground);
+			}
+
+			if (ImGui::CollapsingHeader(this->parameters.camera.getName().c_str(), nullptr, true, true))
+			{
+				if (ofxPreset::Gui::AddParameter(this->parameters.camera.mouseEnabled))
+				{
+					this->parameters.camera.mouseEnabled.update();
+					this->loadCamera();
+				}
+				if (ImGui::Button("Save..."))
+				{
+					auto dialogResult = ofSystemSaveDialog("camera.json", "Save Camera");
+					if (dialogResult.bSuccess)
+					{
+						this->parameters.camera.filePath = dialogResult.filePath;
+						this->saveCamera();
+					}
+				}
+				ImGui::SameLine();
+				if (ImGui::Button("Load..."))
+				{
+					auto dialogResult = ofSystemLoadDialog("Load Camera", false, ofToDataPath("settings"));
+					if (dialogResult.bSuccess)
+					{
+						this->parameters.camera.filePath = dialogResult.filePath;
+						this->loadCamera();
+					}
+				}
+			}
+
+			if (ImGui::CollapsingHeader(this->parameters.mesh.getName().c_str(), nullptr, true, true))
+			{
+				ofxPreset::Gui::AddParameter(this->parameters.mesh.enabled);
+				ofxPreset::Gui::AddRange("Size Range", this->parameters.mesh.sizeMin, this->parameters.mesh.sizeMax, 1.0f);
+				ofxPreset::Gui::AddParameter(this->parameters.mesh.speed);
+				ImGui::Text("Size: %.2f", this->cubeSize);
+			}
+
+			if (ImGui::CollapsingHeader(this->parameters.render.getName().c_str(), nullptr, true, true))
+			{
+				if (ImGui::Button("Load Image..."))
+				{
+					auto dialogResult = ofSystemLoadDialog("Load Image", false, ofToDataPath(""));
+					if (dialogResult.bSuccess)
+					{
+						this->loadImage(dialogResult.filePath);
+					}
+				}
+
+				ImGui::SetNextTreeNodeOpened(true, ImGuiSetCond_Appearing);
+				if (ImGui::TreeNode("Fill"))
+				{
+					ImGui::Columns(3);
+					ImGui::RadioButton("None", this->parameters.render.fillMode.getRef(), RenderModeNone); ImGui::NextColumn();
+					ImGui::RadioButton("Color", this->parameters.render.fillMode.getRef(), RenderModeColor); ImGui::NextColumn();
+					ImGui::RadioButton("Texture", this->parameters.render.fillMode.getRef(), RenderModeTexture); ImGui::NextColumn();
+					ImGui::Columns(1);
+
+					ImGui::TreePop();
+				}
+
+				ImGui::SetNextTreeNodeOpened(true, ImGuiSetCond_Appearing);
+				if (ImGui::TreeNode("Stroke"))
+				{
+					ImGui::Columns(3);
+					ImGui::RadioButton("None", this->parameters.render.strokeMode.getRef(), RenderModeNone); ImGui::NextColumn();
+					ImGui::RadioButton("Color", this->parameters.render.strokeMode.getRef(), RenderModeColor); ImGui::NextColumn();
+					ImGui::RadioButton("Texture", this->parameters.render.strokeMode.getRef(), RenderModeTexture); ImGui::NextColumn();
+					ImGui::Columns(1);
+
+					ImGui::TreePop();
+				}
+			}
+		}
+		this->mouseOverGui = ofxPreset::Gui::EndWindow(windowPos, windowSize);
+	}
+	this->gui.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::loadSettings(const string & filePath)
+{
+	ofFile file(filePath);
+	if (file.exists())
+	{
+		nlohmann::json json;
+		file >> json;
+
+		ofxPreset::Serializer::Deserialize(json, this->parameters);
+
+		this->loadCamera();
+
+		if (!this->parameters.render.imagePath.get().empty())
+		{
+			this->loadImage(this->parameters.render.imagePath);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::saveSettings(const string & filePath)
+{
+	nlohmann::json json;
+
+	ofxPreset::Serializer::Serialize(json, this->parameters);
+
+	ofFile file(filePath, ofFile::WriteOnly);
+	file << json;
+}
+
---
+++ example/src/ofApp.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxImGui.h"
+#include "ofxPreset.h"
+
+class ofApp
+	: public ofBaseApp
+{
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	// Camera
+	void saveCamera();
+	void loadCamera();
+
+	ofEasyCam camera;
+
+	// Mesh
+	float stepper;
+	float cubeSize;
+
+	// Render
+	bool loadImage(const string & filePath);
+
+	ofTexture texture;
+
+	// GUI
+	void imGui();
+
+	ofxImGui gui;
+	bool guiVisible;
+	bool mouseOverGui;
+
+	// Parameters
+	void loadSettings(const string & filePath);
+	void saveSettings(const string & filePath);
+
+	enum RenderMode
+	{
+		RenderModeNone = 0,
+		RenderModeColor,
+		RenderModeTexture
+	};
+
+	struct : ofParameterGroup
+	{
+		struct : ofParameterGroup
+		{
+			ofxPreset::Parameter<ofFloatColor> background{ "Background", ofFloatColor::black };
+			ofxPreset::Parameter<ofFloatColor> foreground{ "Foreground", ofFloatColor::crimson };
+
+			PARAM_DECLARE("Colors", background, foreground);
+		} colors; 
+		
+		struct : ofParameterGroup
+		{
+			ofxPreset::Parameter<bool> mouseEnabled{ "Mouse Enabled", true, true };
+			ofParameter<string> filePath{ "File Path", "camera.json" };
+
+			PARAM_DECLARE("Camera", mouseEnabled, filePath);
+		} camera;
+
+		struct : ofParameterGroup
+		{
+			ofxPreset::Parameter<bool> enabled{ "Enabled", true };
+			ofxPreset::Parameter<float> sizeMin{ "Size Min", 10.0f, 0.0f, 1000.0f };
+			ofxPreset::Parameter<float> sizeMax{ "Size Max", 200.0f, 0.0f, 1000.0f };
+			ofxPreset::Parameter<float> speed{ "Speed", 0.1f, 0.0f, 1.0f };
+
+			PARAM_DECLARE("Mesh", enabled, sizeMin, sizeMax, speed);
+		} mesh;
+
+		struct : ofParameterGroup
+		{
+			ofxPreset::Parameter<int> fillMode{ "Fill Mode", RenderModeTexture };
+			ofxPreset::Parameter<int> strokeMode{ "Stroke Mode", RenderModeNone };
+			ofParameter<string> imagePath{ "Image Path", "texture.jpg" };
+
+			PARAM_DECLARE("Render", fillMode, strokeMode, imagePath);
+		} render;
+
+		PARAM_DECLARE("App", colors, camera, mesh, render);
+	} parameters;
+};
